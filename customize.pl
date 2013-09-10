554b6f4c3a4e7fe5cc64a0da9fabe69c1e479728
Fwiffo <borgified@gmail.com>
2013-09-10 16:18:44 -0700

    extra instructions on how to add bookmarklet + youtube video

--- customize.pl
+++ customize.pl
@@ -8,7 +8,7 @@ my $name = param('name');
 
 print header,start_html;
 
-print "<h3>drag the customized link below to your bookmarks bar</h3>";
+print "<h3>1. drag the customized link (bookmarklet) below to your bookmarks bar</h3>";
 
 print qq|<a href="javascript: { document.getElementById('CF00N30000006gryA').value = 'CLOSED'; document.getElementById('CF00N30000006gry6').value = '|;
 
@@ -16,5 +16,10 @@ print $name;
 
 print qq|'; document.getElementById('00N30000008kB6x').value = '5'; document.getElementById('00N30000006gryZ').value = 'fixed'; var x = document.getElementsByClassName('dateFormat')[0].innerHTML; var start = x.search(/>/); var end = x.search(/a>/); var date = x.slice(start+1,end-2); document.getElementById('00N30000006gryP').value = date; void(0); }">CLOSE TICKET</a>|;
 
+print "<h3>2. edit an incident (ticket)</h3>";
+print "<h3>3. click on bookmarklet in your bookmarks toolbar to populate required fields</h3>";
+print "<h3>4. click save in ticket</h3>";
+
+print "<br><br>extra help: youtube <a href='http://www.youtube.com/watch?v=3YxX8E4iAlI'>video</a> shows how to drag bookmarklet";
 
 print end_html;
