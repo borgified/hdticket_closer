4506382841c658137a939711f2c741a07cd5a448
Fwiffo <borgified@gmail.com>
2013-09-10 15:56:54 -0700

    all sorts of crazy acrobatics to get quote and doublequotes printed out right for browser

--- customize.pl
+++ customize.pl
@@ -10,9 +10,11 @@ print header,start_html;
 
 print "<h3>drag the customized link below to your bookmarks bar</h3>";
 
-print <<EOF
-<a href='javascript: { document.getElementById("CF00N30000006gryA").value = "CLOSED"; document.getElementById("CF00N30000006gry6").value = "$name"; document.getElementById("00N30000008kB6x").value = "5"; document.getElementById("00N30000006gryZ").value = "fixed"; var now = new Date(); function AddZero(num) { return (num >= 0 && num < 10) ? "0" + num : num + ""; }; function FixHour(num) { return (num >= 13 ? num - 12 : num); }; var strDateTime = [[now.getMonth() + 1, now.getDate(), now.getFullYear()].join("/"), [FixHour(now.getHours()), AddZero(now.getMinutes())].join(":"), now.getHours() >= 12 ? "PM" : "AM"].join(" "); document.getElementById("00N30000006gryP").value = strDateTime; void(0); }'>CLOSE TICKET</a>
-EOF
-;
+print qq|<a href="javascript: { document.getElementById('CF00N30000006gryA').value = 'CLOSED'; document.getElementById('CF00N30000006gry6').value = '|;
+
+print $name;
+
+print qq|'; document.getElementById('00N30000008kB6x').value = '5'; document.getElementById('00N30000006gryZ').value = 'fixed'; var x = document.getElementsByClassName('dateFormat')[0].innerHTML; var start = x.search(/>/); var end = x.search(/a>/); var date = x.slice(start+1,end-2); document.getElementById('00N30000006gryP').value = date; void(0); }">CLOSE TICKET</a>|;
+
 
 print end_html;
